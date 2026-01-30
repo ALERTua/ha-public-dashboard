dec282d20632e52c0f7bb7b3c09e961876db92f2
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2026-01-30 16:35:22 +0200

    feat: Add Vite base path config
    - Add base path configuration to Vite build settings.

--- public-dashboard/rootfs/var/www/vite.config.mjs
+++ public-dashboard/rootfs/var/www/vite.config.mjs
@@ -17,6 +17,8 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    // Set base to handle both direct access and ingress paths
+    base: './'
   }
 })
